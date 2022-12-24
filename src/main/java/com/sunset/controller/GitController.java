dee5a7e13f1e536cb536e3a8b145618396391090
sunset <740662306@qq.com>
2022-12-24 22:26:32 +0800

    Idea集成Git第2次提交

--- src/main/java/com/sunset/controller/GitController.java
+++ src/main/java/com/sunset/controller/GitController.java
@@ -11,7 +11,9 @@ public class GitController {
     @GetMapping
     public String getById(){
         System.out.println("this is a git demo,welcome learn idea and Git");
+        System.out.println("Git version 2");
         return "this is a git demo,welcome learn idea and Git";
+
     }
 
 }
