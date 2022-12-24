053bf43b7de57176ddefd6e39713c41587bfb682
sunset <740662306@qq.com>
2022-12-24 22:17:55 +0800

    Idea集成Git第一次提交

---
+++ src/main/java/com/sunset/controller/GitController.java
@@ -0,0 +1,17 @@
+package com.sunset.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/git")
+public class GitController {
+
+    @GetMapping
+    public String getById(){
+        System.out.println("this is a git demo,welcome learn idea and Git");
+        return "this is a git demo,welcome learn idea and Git";
+    }
+
+}
