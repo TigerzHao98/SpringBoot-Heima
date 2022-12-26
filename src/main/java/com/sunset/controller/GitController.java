37f9b7da53dd7d19c90c9e3ddac5c9ae1196a3a0
Tiger_zHao <740662306@qq.com>
2022-12-26 04:18:16 +0000

    update src/main/java/com/sunset/controller/GitController.java. IDEA集成Gitee进行push
    Signed-off-by: Tiger_zHao <740662306@qq.com>

--- src/main/java/com/sunset/controller/GitController.java
+++ src/main/java/com/sunset/controller/GitController.java
@@ -18,6 +18,7 @@ public class GitController {
         System.out.println("hot-fix 演示合并冲突");
         System.out.println("IDEA集成Github进行push");
         System.out.println("IDEA集成Github进行puLL");
+        System.out.println("IDEA集成Gitee进行push");
         return "this is a git demo,welcome learn idea and Git";
 
     }
